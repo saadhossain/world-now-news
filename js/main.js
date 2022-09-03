bf1e26e53b93f361865b1f227ada01ad2cfecf07
saadhossain <mshsarkar@gmail.com>
2022-09-03 22:46:15 +0600

    News Sorting based on low views to high, add neccessary comments added, unavailable replaced by Not Found and add sort dropdown to html file

--- js/main.js
+++ js/main.js
@@ -10,23 +10,27 @@ const loadCategory = async() =>{
     }
 }
 const displayCategory = (categories) => {
+    //Get the category menu container for pc/laptop
     const newsCategoryPc = document.getElementById('category-container-pc');
+    //Get the category menu for mobile collapsible menu/for responsive menu
     const newsCategoryMobile = document.getElementById('category-container-mobile');
     categories.forEach(category => {
+        //Create li for adding every single category for Desktop/laptop
         const categoryLi = document.createElement('li');
         categoryLi.innerHTML = `
             <a href="#" id="category-name" onclick="loadNews('${category.category_id}')">${category.category_name}</a>
         `
+        //Create li for adding every single category for Mobile Menu
         const categoryLiMobile = document.createElement('li');
         categoryLiMobile.innerHTML = `
             <a href="#" id="category-name" onclick="loadNews('${category.category_id}')">${category.category_name}</a>
         `
+        //Append for Desktop/Laptop
         newsCategoryPc.appendChild(categoryLi);
+        //Append for Mobile Category Menu
         newsCategoryMobile.appendChild(categoryLiMobile);
     });
 }
-
-
 loadCategory()
 //Load News from Category
 const loadNews = async (newsCat) => {
@@ -62,6 +66,8 @@ const displayNews = (allNews) => {
     //Get the News Container
     const newsContainer = document.getElementById('news-container');
     newsContainer.innerHTML = '';
+    //Sorting the News by Total Views from Low to high
+    allNews.sort((a ,b ) => a.total_view - b.total_view);
     allNews.forEach(news => {
         //Single news
         const newsSingle =  document.createElement('div');
@@ -70,7 +76,9 @@ const displayNews = (allNews) => {
         <figure><label class="w-full cursor-pointer" for="news-modal" onclick="loadDetails('${news._id}')"><img src="${news.thumbnail_url}" class="h-64 w-full"></label></figure>
         <div class="card-body w-full md:w-3/4 p-3 md:p-5">
             <label class="card-title cursor-pointer" for="news-modal" onclick="loadDetails('${news._id}')">${news.title}</label>
+            <!----------------Slice 300 word to showing on desktop/laptop---------------------->
             <p class="hidden md:block">${news.details.length > 300 ? news.details.slice(0, 300) + '...' : news.details}</p>
+            <!----------------Slice 200 word to showing on Mobile---------------------->
             <p class="block md:hidden">${news.details.length > 200 ? news.details.slice(0, 200) + '...' : news.details}</p>
             <!-------------Post Details--------------------->
             <div class="flex justify-between">
@@ -82,7 +90,7 @@ const displayNews = (allNews) => {
                     </div>
                 </div>
                 <div>
-                    <h3 class="text-xl"><span><i class="fa-solid fa-eye"></i></span> ${news.total_view ? news.total_view: 'Unavailable'}</h3>
+                    <h3 class="text-xl"><span><i class="fa-solid fa-eye"></i></span> ${news.total_view ? news.total_view: 'Not Found'}</h3>
                 </div>
                 <div>
                     <label for="news-modal" class="cursor-pointer" onclick="loadDetails('${news._id}')"><span class="text-xl text-violet-500"><i class="fa-solid fa-right-long"></i></span></label>
@@ -94,8 +102,7 @@ const displayNews = (allNews) => {
         //Stop the spinner after append the news
         spinner.classList.add('hidden');
         
-    });
-                    
+    });       
 }
 const loadDetails = async(newsID) => {
     const url = `https://openapi.programming-hero.com/api/news/${newsID}`;
